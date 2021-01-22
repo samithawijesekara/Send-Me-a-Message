e64731aaa2797a934d0156bf2f321e2533dc2321
samithawijesekara@gmail.com <samithawijesekara@gmail.com>
2021-01-23 00:29:35 +0530

    added files

---
+++ script.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycby_T62R86bx-FXYZVr6Iu1Z2eZ8vaG6jgEpaktLR7UWL6ik-6bVzTto/exec' //add here google sheets deploying web app url
+const form = document.forms['google-sheet']
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+        .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+        .catch(error => console.error('Error!', error.message))
+})
